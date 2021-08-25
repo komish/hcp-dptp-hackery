7dde59725a5e64ee92e5b2242645c3cd05869ce5
Jose R. Gonzalez <josegonzalez89@gmail.com>
2021-08-25 17:11:56 -0500

    Wed Aug 25 17:11:56 CDT 2021

--- step-registry/redhat-openshift-ecosystem/preflight/common/run-preflight.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO... make more robust.
-
-echo "Starting demo execution of preflight..."
-export PFLT_LOGLEVEL=trace
-which preflight &>/dev/null && preflight check operator "${PREFLIGHT_TEST_ASSET}"
-echo "Ending demo execution of preflight..."
